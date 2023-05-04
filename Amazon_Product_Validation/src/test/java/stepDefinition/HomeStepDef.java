1a186125f1d4248f9f99341b07c871eef88089ec
akash01847 <124980188+akash01847@users.noreply.github.com>
2023-05-04 17:04:08 +0530

    Update HomeStepDef.java

--- Amazon_Product_Validation/src/test/java/stepDefinition/HomeStepDef.java
+++ Amazon_Product_Validation/src/test/java/stepDefinition/HomeStepDef.java
@@ -24,22 +24,22 @@ public class HomeStepDef {
     }
 
     @When("I open the product page for specified productId")
-    public void i_open_the_product_page_for_specified_product_id() throws PendingException, IOException {
+    public void i_open_the_product_page_for_specified_product_id() throws PendingException {
         Assert.assertTrue(tcs.pom.getHome().navigateToProductPage());
     }
 
     @Then("I should see the Buy Now button")
-    public void i_should_see_the_button() throws PendingException, IOException {
+    public void i_should_see_the_button() throws PendingException {
         Assert.assertTrue(tcs.pom.getHome().validateBuyNowButton());
     }
 
     @Then("I should check if customer rating for the product is over four")
-    public void i_should_check_if_customer_rating_for_the_product_is_over_four() throws PendingException, IOException {
+    public void i_should_check_if_customer_rating_for_the_product_is_over_four() throws PendingException{
         tcs.pom.getHome().checkCustomerRatings();
     }
 
     @Then("Print all the offers available on the description page for the product")
-    public void print_all_the_offers_available_on_the_description_page_for_the_product() throws PendingException, IOException {
+    public void print_all_the_offers_available_on_the_description_page_for_the_product() throws PendingException {
         Assert.assertTrue(tcs.pom.getHome().offerDetails());
     }
 }
