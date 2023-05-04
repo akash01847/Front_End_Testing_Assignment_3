aaff613297f07bf917da8a4ad4b8621e490fa661
akash01847 <124980188+akash01847@users.noreply.github.com>
2023-05-04 23:42:06 +0530

    Update Hooks.java

--- Amazon_Product_Validation/src/test/java/stepDefinition/Hooks.java
+++ Amazon_Product_Validation/src/test/java/stepDefinition/Hooks.java
@@ -21,16 +21,4 @@ public class Hooks {
     public void tearDown() throws IOException {
         tcs.base.initializeDriver().quit();
     }
-
-
-
-    /*@BeforeAll
-    public static void beforeAll() throws IOException {
-        Hooks hooks=new Hooks(new TestContextSetup());
-        hooks.closeFooterNotificationInCode();
-    }
-
-    public void closeFooterNotificationInCode() throws IOException {
-        tcs.pom.getFooter().closeFooterPopup();
-    }*/
 }
